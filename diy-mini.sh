432e1aa628c020929d79862a683d6e051549f2e5
wtcoolboy2023 <wtcoolboy@163.com>
2026-09-04 16:24:29 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -38,7 +38,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-
+git clone --depth=1  https://github.com/gdy666/luci-app-lucky.git package/lucky
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
